471e8ebad66a518be5b3eb02649f6b56dcd36b58
sho <hira.alpherz@gmail.com>
2017-08-24 18:47:08 +0900

    add install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for f in .??*
+do 
+  [[ "$f" == ".git" ]] && continue
+  [[ "$f" == ".DS_Store" ]] && continue
+
+  path=$(cd $(dirname $f) && pwd)
+  path="${path}/${f}"
+  echo $path
+  ln -s $path ../
+done

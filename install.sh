989a3dde58a10b0b573f1a757380b59549fac69b
sho <sho@shonoMacBook-Pro.local>
2017-10-07 13:19:23 +0900

    for mac

--- install.sh
+++ install.sh
@@ -1,12 +1,29 @@
-
 # deploy dot files
 
+DOTFILES_DIR="dotfiles"
 script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
-dotfiles_dir=$script_dir'/dotfiles/.'
+dotfiles_dir=$script_dir'/'$DOTFILES_DIR
 target_dir=$HOME'/'
-cp -rs $dotfiles_dir $target_dir 
 
-# init apps 
-
-
+if [ $(uname) = "Darwin" ]; then
+	# for file in $( ls -a $dotfiles_dir); do
+	# 	if [ $file = "." ] || [ $file = ".." ]; then
+	# 		continue;
+	# 	fi
+	# 	if [ -a $HOME/$file ]; then
+	# 		echo "exists"
+	# 	else
+	# 		echo "ln"
+	# 	fi
+	# done
+	for file in $( find $DOTFILES_DIR -type f ) ; do
+		filepath=${file#$DOTFILES_DIR/}
+		ppath=$(dirname $HOME/$filepath)
+		echo $ppath
+		mkdir -p $ppath
+	 	ln -s $(pwd $(dirname $file))/$file $HOME/$filepath
+	done 
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+	cp -rs $dotfiles_dir $target_dir 
+fi
 

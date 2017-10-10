066ddd1fb5488ecc8173766549e20123b3c96008
sho <sho@shonoMacBook-Pro.local>
2017-10-10 12:47:26 +0900

    fzy

--- init/linux.sh
+++ init/linux.sh
@@ -2,6 +2,9 @@ sudo apt-get -y install git
 sudo apt-get -y install zsh
 sudo chsh /usr/bin/zsh
 
+wget https://github.com/jhawthorn/fzy/releases/download/0.9/fzy_0.9-1_amd64.deb
+sudo dpkg -i fzy_0.9-1_amd64.deb
+
 sudo apt-get -y install python3-pip
 pip3 install --upgrade pip
 
@@ -14,5 +17,4 @@ sudo apt-get update
 sudo apt-get -y install neovim
 
 pip3 install neovim
-
 pip3 install neovim-remote
